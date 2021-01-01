75ab6a0557a988a9b4eb51602b48dc61d732b330
Andrew Macheret <andrew.macheret@gmail.com>
2020-12-31 18:38:10 -0800

    Fix colors better

--- www/index.js
+++ www/index.js
@@ -296,7 +296,8 @@ function loadSettings(data) {
         if (fieldValue !== '') {
           fieldValues.push(fieldValue);
           const color = colors[r][c];
-          const colorString = '#' + ['red', 'green', 'blue'].map(k => Math.round((color[k] || 0) * 255).toString(16)).join('');
+          const colorString = '#' + ['red', 'green', 'blue'].map(k =>
+                              Math.round((color[k] || 0) * 255).toString(16).padStart(2, '0')).join('');
           fieldColors.push(colorString);
         }
       }
